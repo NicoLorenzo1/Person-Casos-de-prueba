98d151de1f63508d9fcc47fa9638159ddaeb16d7
NicoLorenzo1 <88661584+NicoLorenzo1@users.noreply.github.com>
2021-09-08 20:39:04 -0300

    Person con test finalizados

--- test/Library.Test/PersonTests.cs
+++ test/Library.Test/PersonTests.cs
@@ -13,9 +13,72 @@ namespace Tests
         }
 
         [Test]
-        public void Test1() // Cambiá el nombre para indicar qué estás probando
+        public void TestType() // Cambiá el nombre para indicar qué estás probando
         {
-            // Insertá tu código  de pruebaaquí
+            string value = "identification";
+            bool result = IdUtils.IdIsValid(value);
+            Assert.AreEqual(false, result);
+        }
+
+        [Test]
+        public void TestValidLength() 
+        {
+            string value = "1111111-1";
+            bool result = IdUtils.IdIsValid(value);
+            Assert.AreEqual(true, result);
+        }
+
+        [Test]
+        public void TestShortLength() 
+        {
+            string value = "111111-1";
+            bool result = IdUtils.IdIsValid(value);
+            Assert.False(result); //como el resultado es bool se puede usar Assert.false
+        }
+
+        [Test]
+        public void TestMissingDash() 
+        {
+            string value = "11111111";
+            bool result = IdUtils.IdIsValid(value);
+            Assert.True(result);
+        }
+
+        [Test]
+        public void TestDashWrongPosition() 
+        {
+            string value = "111-11111";
+            bool result = IdUtils.IdIsValid(value);
+            Assert.True(result);
+        }
+
+        [Test]
+        public void TestNameNull() 
+        {
+           Person persona1 = new Person("", "1111111-1");
+           Assert.AreEqual(null, persona1.Name);
+        }
+
+        [Test]
+        public void TestValidName() 
+        {
+           Person persona1 = new Person("Juan Lopez", "1111111-1");
+           Assert.AreEqual("Juan Lopez", persona1.Name);
+        }
+        
+        [Test]
+        public void TestName() 
+        {
+           Person persona1 = new Person(null, "1111111-1");
+           Assert.AreEqual(null, persona1.Name);
+        }
+
+        [Test]
+        public void TestChangeName() 
+        {
+           Person persona1 = new Person("Juan Lopez", "1111111-1");
+           persona1.Name = "Sebastian Fernandez";
+           Assert.AreEqual("Sebastian Fernandez", persona1.Name);
         }
     }
 }
